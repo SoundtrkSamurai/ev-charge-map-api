c8027325bda3cc9a1f33e5dde05a73c2cdab4af4
Allen Taylor <Allen.Taylor@esentire.com>
2023-07-20 19:55:11 -0700

    db-init: Add server route for storing routes

--- app/Http/Controllers/Api/Route/RouteController.php
+++ app/Http/Controllers/Api/Route/RouteController.php
@@ -2,33 +2,34 @@
 
 namespace App\Http\Controllers\Api\Route    ;
 
+use App\Http\Controllers\Controller;
 use App\Models\Route;
 use Illuminate\Http\Request;
 
 class RouteController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $route = new Route;
+
+            $route->name = $request->get('name');
+            $route->from = $request->get('from');
+            $route->to = $request->get('to');
+            $route->range = $request->get('range');
+
+            $route->save();
+
+            return response()->json([
+                'message' => 'The reoute was added successfully.',
+                'route' => $route
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Something went wrong saving the route.'], 400);
+        }
     }
 
     /**
